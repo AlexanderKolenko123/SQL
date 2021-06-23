82164001c89e8ab1247a735f921aa8447f1227a6
MyRepos <alexander.kolenko@gmail.com>
2021-06-23 19:48:08 +0300

    Ex2-3

---
+++ Lesson2/Ex2/create_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE users (
+  id INT UNSIGNED,
+  name VARCHAR(255) COMMENT 'Название раздела'
+) COMMENT = 'таблица пользователей';
---
+++ Lesson2/Ex3/test.bat
@@ -0,0 +1,2 @@
+C:\Program Files\MySQL\MySQL Server 8.0\bin>mysqldump.exe -u root -p example > c:\temp\sample
+C:\Program Files\MySQL\MySQL Server 8.0\bin>mysql.exe -u root -p sample < c:\temp\sample.sql
